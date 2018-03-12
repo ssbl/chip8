dfb23a88c45c231a4d58d8e72b9ab225d3ac9118
Shubham Lagwankar <shubhu105@gmail.com>
2018-03-12 18:21:53 -0400

    Detect key presses

--- src/main.cpp
+++ src/main.cpp
@@ -81,3 +81,66 @@ cleanup:
   SDL_DestroyWindow(window);
   SDL_Quit();
 }
+      if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) {
+        const auto keyval = event.type == SDL_KEYUP ? 0 : 1;
+        switch (event.key.keysym.sym) {
+          case SDLK_1: // 1
+            chip8.key[0x1] = keyval;
+            break;
+          case SDLK_2: // 2
+            chip8.key[0x2] = keyval;
+            break;
+          case SDLK_3: // 3
+            chip8.key[0x3] = keyval;
+            break;
+          case SDLK_4: // A
+            chip8.key[0xc] = keyval;
+            break;
+          case SDLK_q: // 4
+            chip8.key[0x4] = keyval;
+            break;
+          case SDLK_w: // 5
+            chip8.key[0x5] = keyval;
+            break;
+          case SDLK_e: // 6
+            chip8.key[0x6] = keyval;
+            break;
+          case SDLK_r: // D
+            chip8.key[0xd] = keyval;
+            break;
+          case SDLK_a: // 7
+            chip8.key[0x7] = keyval;
+            break;
+          case SDLK_s: // 8
+            chip8.key[0x8] = keyval;
+            break;
+          case SDLK_d: // 9
+            chip8.key[0x9] = keyval;
+            break;
+          case SDLK_f: // E
+            chip8.key[0xe] = keyval;
+            break;
+          case SDLK_z: // A
+            chip8.key[0xa] = keyval;
+            break;
+          case SDLK_x: // 0
+            chip8.key[0x0] = keyval;
+            break;
+          case SDLK_c: // B
+            chip8.key[0xb] = keyval;
+            break;
+          case SDLK_v: // F
+            chip8.key[0xf] = keyval;
+            break;
+          default:
+            break;
+        }
+      }
+    }
+
+    if (chip8.draw_flag) {
+      SDL_RenderClear(renderer);
+      auto copy = chip8.framebuffer;
+      SDL_UpdateTexture(texture, nullptr, copy.data(), 64 * sizeof(unsigned));
+      SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+      SDL_RenderPresent(renderer);
