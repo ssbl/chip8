7615752eaf595cb1fe713b90ff79f570a86362ef
Shubham Lagwankar <shubhu105@gmail.com>
2018-04-30 15:23:06 -0400

    Use value_type to get the array element type

--- src/main.cpp
+++ src/main.cpp
@@ -74,8 +74,10 @@ main(const int argc, const char* argv[])
 
     if (chip8.draw_flag) {
       SDL_RenderClear(renderer);
-      SDL_UpdateTexture(
-        texture, nullptr, chip8.framebuffer.data(), 64 * sizeof(unsigned));
+      SDL_UpdateTexture(texture,
+                        nullptr,
+                        chip8.framebuffer.data(),
+                        64 * sizeof(decltype(chip8.framebuffer)::value_type));
       SDL_RenderCopy(renderer, texture, nullptr, nullptr);
       SDL_RenderPresent(renderer);
     }
